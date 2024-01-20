f33b01ebbd166183fb007240233074ba68774442
aayush <aayushlad058@gmail.com>
2024-01-20 18:11:31 +0530

    axios update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './components/App'
 import './index.css'
 import axios from 'axios'
-axios.defaults.baseURL = "http://localhost:8080"
+axios.defaults.baseURL = "https://notes-app-2z2s.vercel.app"
 axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
