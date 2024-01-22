8cc7e4b74099f22074ad5d3ab9a56188292bf646
aayush <aayushlad058@gmail.com>
2024-01-22 17:45:38 +0530

    cookie error solved

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -35,8 +35,8 @@ router.post("/login", async (req, res) => {
             expires: new Date(exp),
             httpOnly: true,
             sameSite: "None",
-            domain: "notes-app-frontend-pi.vercel.app",
-            // path: "/",
+            // domain: "notes-app-frontend-pi.vercel.app",
+            path: "/",
             secure: process.env.NODE_ENV === "production",
         })
 
@@ -53,8 +53,8 @@ router.get("/logout", (req, res) => {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
             sameSite: "None",
-            domain: "notes-app-frontend-pi.vercel.app",
-            // path: "/"
+            // domain: "notes-app-frontend-pi.vercel.app",
+            path: "/"
         })
         res.sendStatus(200)
     } catch (err) {
